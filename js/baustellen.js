73ffe7e0693c4f73ebc49e82316cb18b65caefe4
grospitz-wreck-it <grospitz@gmail.com>
2026-03-13 22:06:04 +0100

    Update baustellen.js

--- js/baustellen.js
+++ js/baustellen.js
@@ -1,1 +1,26 @@
 
+async function loadBaustellen(){
+
+try{
+
+const response = await fetch(
+"https://baustellen.strassen.nrw/api"
+);
+
+const data = await response.json();
+
+L.geoJSON(data,{
+style:{
+color:"orange"
+}
+}).addTo(map);
+
+}catch(e){
+
+console.log("Baustellen konnten nicht geladen werden");
+
+}
+
+}
+
+loadBaustellen();
