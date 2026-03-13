11282623975c5c8f40c42b37537c7ac3ef48e676
grospitz-wreck-it <grospitz@gmail.com>
2026-03-13 21:46:44 +0100

    Update traffic.js

--- js/traffic.js
+++ js/traffic.js
@@ -1,1 +1,31 @@
 
+let trafficLayer;
+
+async function loadTraffic(){
+
+if(trafficLayer){
+map.removeLayer(trafficLayer);
+}
+
+const response = await fetch("data/traffic.geojson");
+const data = await response.json();
+
+trafficLayer = L.geoJSON(data,{
+style:{
+color:"red",
+weight:4
+},
+onEachFeature:function(feature,layer){
+
+layer.bindPopup(
+"<b>"+feature.properties.title+"</b>"
+);
+
+}
+}).addTo(map);
+
+}
+
+loadTraffic();
+
+setInterval(loadTraffic,120000);
