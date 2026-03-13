53d241d9e7d9327c32f554f4f8c66dc017f11007
grospitz-wreck-it <grospitz@gmail.com>
2026-03-13 21:53:28 +0100

    Update icons.js

--- js/icons.js
+++ js/icons.js
@@ -1,1 +1,19 @@
 
+const icons = {
+
+baustelle: L.icon({
+iconUrl:"https://cdn-icons-png.flaticon.com/512/565/565547.png",
+iconSize:[28,28]
+}),
+
+unfall: L.icon({
+iconUrl:"https://cdn-icons-png.flaticon.com/512/684/684908.png",
+iconSize:[28,28]
+}),
+
+gefahr: L.icon({
+iconUrl:"https://cdn-icons-png.flaticon.com/512/565/565491.png",
+iconSize:[28,28]
+})
+
+}
