e786329e0f43230170987e4c021c2825a9e75ce2
grospitz-wreck-it <grospitz@gmail.com>
2026-03-13 22:36:44 +0100

    Update build_traffic.py

--- scripts/build_traffic.py
+++ scripts/build_traffic.py
@@ -1,45 +1,59 @@
 import json
 import requests
 
-overpass_query = """
+bbox = "(51.98,8.45,52.25,8.90)"
+
+query = f"""
 [out:json][timeout:25];
 (
-  way["highway"="construction"](51.98,8.45,52.25,8.90);
-  node["highway"="construction"](51.98,8.45,52.25,8.90);
+  way["highway"="construction"]{bbox};
+  node["highway"="construction"]{bbox};
 );
 out center;
 """
 
 url = "https://overpass-api.de/api/interpreter"
 
-r = requests.post(url,data=overpass_query)
-
-data = r.json()
+headers = {
+"User-Agent": "herford-traffic-map"
+}
 
 features=[]
 
-for e in data["elements"]:
+try:
 
-    if "lat" in e:
-        lat=e["lat"]
-        lon=e["lon"]
-    else:
-        lat=e["center"]["lat"]
-        lon=e["center"]["lon"]
+    r = requests.post(url,data=query,headers=headers,timeout=60)
 
-    features.append({
-        "type":"Feature",
-        "properties":{
-            "title":"Baustelle",
-            "type":"baustelle"
-        },
-        "geometry":{
-            "type":"Point",
-            "coordinates":[lon,lat]
-        }
-    })
+    if r.status_code != 200:
+        raise Exception("Overpass error")
 
-geojson={
+    data = r.json()
+
+    for e in data["elements"]:
+
+        if "lat" in e:
+            lat = e["lat"]
+            lon = e["lon"]
+        else:
+            lat = e["center"]["lat"]
+            lon = e["center"]["lon"]
+
+        features.append({
+            "type":"Feature",
+            "properties":{
+                "title":"Baustelle",
+                "type":"baustelle"
+            },
+            "geometry":{
+                "type":"Point",
+                "coordinates":[lon,lat]
+            }
+        })
+
+except Exception as err:
+    print("Fehler beim Laden der Baustellen:",err)
+
+geojson = {
 "type":"FeatureCollection",
 "features":features
 }
