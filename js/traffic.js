19874775d0c30a1d393441ba95e27fbb894c9ded
grospitz-wreck-it <grospitz@gmail.com>
2026-03-13 22:00:09 +0100

    Update traffic.js

--- js/traffic.js
+++ js/traffic.js
@@ -1,27 +1,85 @@
 let trafficLayer;
 
+// Bounding Box Kreis Herford
+const HERFORD_BBOX = {
+minLat: 52.00,
+maxLat: 52.25,
+minLon: 8.50,
+maxLon: 8.90
+};
+
+function inHerford(lat, lon){
+
+return (
+lat > HERFORD_BBOX.minLat &&
+lat < HERFORD_BBOX.maxLat &&
+lon > HERFORD_BBOX.minLon &&
+lon < HERFORD_BBOX.maxLon
+);
+
+}
+
 async function loadTraffic(){
 
 if(trafficLayer){
 map.removeLayer(trafficLayer);
 }
 
-const res = await fetch("data/traffic.geojson");
-const data = await res.json();
+try{
 
-trafficLayer = L.geoJSON(data,{
+// Beispiel API (GeoJSON Traffic Events)
+const response = await fetch(
+"https://verkehr.autobahn.de/o/autobahn"
+);
+
+const data = await response.json();
+
+let features = [];
+
+data.roads.forEach(road => {
+
+if(!road.events) return;
+
+road.events.forEach(event => {
+
+const lat = event.latitude;
+const lon = event.longitude;
+
+if(inHerford(lat,lon)){
+
+features.push({
+
+"type":"Feature",
+
+"properties":{
+"title":event.description || "Verkehrsmeldung",
+"type":"gefahr",
+"city":"Kreis Herford"
+},
+
+"geometry":{
+"type":"Point",
+"coordinates":[lon,lat]
+}
+
+});
+
+}
+
+});
+
+});
+
+trafficLayer = L.geoJSON({
+
+"type":"FeatureCollection",
+"features":features
+
+},{
 
 pointToLayer:function(feature,latlng){
 
-let icon = icons.baustelle;
-
-if(feature.properties.type==="unfall"){
-icon = icons.unfall;
-}
-
-if(feature.properties.type==="gefahr"){
-icon = icons.gefahr;
-}
+let icon = icons.gefahr;
 
 return L.marker(latlng,{icon:icon});
 
@@ -30,18 +88,23 @@ return L.marker(latlng,{icon:icon});
 onEachFeature:function(feature,layer){
 
 layer.bindPopup(
-
 "<b>"+feature.properties.title+"</b><br>"+
 feature.properties.city
-
 )
 
 }
 
 }).addTo(map);
 
+}catch(err){
+
+console.error("Traffic API Fehler",err);
+
+}
+
 }
 
 loadTraffic();
 
-setInterval(loadTraffic,120000);
+// alle 3 Minuten aktualisieren
+setInterval(loadTraffic,180000);
