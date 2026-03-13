fe13c7237be5d2c46472bcd472ea5821869a8a7a
grospitz-wreck-it <grospitz@gmail.com>
2026-03-13 22:27:01 +0100

    Update traffic.js

--- js/traffic.js
+++ js/traffic.js
@@ -1,26 +1,71 @@
 let trafficLayer;
 
+let stats={
+baustellen:0,
+unfaelle:0,
+gefahren:0
+};
+
+function updateStats(){
+
+document.getElementById("countBaustellen").innerText=stats.baustellen;
+document.getElementById("countUnfaelle").innerText=stats.unfaelle;
+document.getElementById("countGefahr").innerText=stats.gefahren;
+
+}
+
 async function loadTraffic(){
 
 if(trafficLayer){
 map.removeLayer(trafficLayer);
 }
 
-const res = await fetch("data/traffic.geojson");
-const data = await res.json();
+stats={
+baustellen:0,
+unfaelle:0,
+gefahren:0
+};
 
-trafficLayer = L.geoJSON(data,{
+let features=[];
+
+try{
+
+const res=await fetch("data/traffic.geojson");
+const data=await res.json();
+
+data.features.forEach(f=>{
+
+features.push(f);
+
+if(f.properties.type==="baustelle") stats.baustellen++;
+if(f.properties.type==="unfall") stats.unfaelle++;
+if(f.properties.type==="gefahr") stats.gefahren++;
+
+});
+
+}catch(e){
+
+console.log("keine daten");
+
+}
+
+trafficLayer=L.geoJSON({
+
+type:"FeatureCollection",
+features:features
+
+},{
 
 pointToLayer:function(feature,latlng){
 
-let icon = icons.gefahr;
+let icon=icons.gefahr;
 
 if(feature.properties.type==="baustelle"){
-icon = icons.baustelle;
+icon=icons.baustelle;
 }
 
 if(feature.properties.type==="unfall"){
-icon = icons.unfall;
+icon=icons.unfall;
 }
 
 return L.marker(latlng,{icon:icon});
@@ -36,6 +81,8 @@ layer.bindPopup(
 }
 
 }).addTo(map);
+
+updateStats();
 
 }
 
