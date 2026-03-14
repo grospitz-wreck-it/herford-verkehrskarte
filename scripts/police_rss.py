c4cced761eff3ebf1d5c8ef0c93e7b5e5d5cab90
grospitz-wreck-it <grospitz@gmail.com>
2026-03-14 14:21:06 +0100

    Update police_rss.py

--- scripts/police_rss.py
+++ scripts/police_rss.py
@@ -1,1 +1,16 @@
+import feedparser
+import json
 
+feed="https://www.presseportal.de/rss/polizeipresse"
+
+d=feedparser.parse(feed)
+
+items=[]
+
+for e in d.entries[:10]:
+ items.append({
+  "title":e.title
+ })
+
+with open("data/police.json","w") as f:
+ json.dump(items,f)
