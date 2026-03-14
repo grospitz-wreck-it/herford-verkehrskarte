16ec707bb02489e1ee70a2fcb973ec801aae90d8
grospitz-wreck-it <grospitz@gmail.com>
2026-03-14 16:09:27 +0100

    Update build_traffic.py

--- scripts/build_traffic.py
+++ scripts/build_traffic.py
@@ -1,9 +1,50 @@
+import requests
 import json
 
-data={
-"type":"FeatureCollection",
-"features":[]
+query = """
+[out:json][timeout:25];
+
+(
+node["highway"="construction"](52.05,8.45,52.25,8.85);
+way["highway"="construction"](52.05,8.45,52.25,8.85);
+);
+
+out center;
+"""
+
+r = requests.post(
+    "https://overpass-api.de/api/interpreter",
+    data=query
+)
+
+data = r.json()
+
+features = []
+
+for el in data["elements"]:
+
+    if "lat" in el:
+        lat = el["lat"]
+        lon = el["lon"]
+    else:
+        lat = el["center"]["lat"]
+        lon = el["center"]["lon"]
+
+    features.append({
+        "type":"Feature",
+        "geometry":{
+            "type":"Point",
+            "coordinates":[lon,lat]
+        },
+        "properties":{
+            "title":"Baustelle"
+        }
+    })
+
+geojson = {
+    "type":"FeatureCollection",
+    "features":features
 }
 
 with open("data/traffic.json","w") as f:
- json.dump(data,f)
+    json.dump(geojson,f)
