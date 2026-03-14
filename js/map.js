8a3e60a21ee76a05c5711cca3e658866bf718ffd
grospitz-wreck-it <grospitz@gmail.com>
2026-03-14 15:45:55 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -2,13 +2,10 @@ const map = L.map("map").setView([52.114,8.673],11)
 
 L.tileLayer(
 "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-{
-attribution:"© OpenStreetMap"
-}
+{ attribution:"© OpenStreetMap" }
 ).addTo(map)
 
 const cluster = L.markerClusterGroup()
-
 map.addLayer(cluster)
 
 function emojiIcon(e){
@@ -32,3 +29,21 @@ marker.bindPopup(text)
 cluster.addLayer(marker)
 
 }
+
+
+// Kreis laden
+fetch("data/kreis-herford.geojson")
+.then(r=>r.json())
+.then(data=>{
+
+const border=L.geoJSON(data,{
+style:{
+color:"#c4001a",
+weight:3,
+fill:false
+}
+}).addTo(map)
+
+map.fitBounds(border.getBounds())
+
+})
