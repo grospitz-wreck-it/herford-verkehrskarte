51a750815cf771484f73d1abb588314f9b1c52d0
grospitz-wreck-it <grospitz@gmail.com>
2026-03-14 14:21:23 +0100

    Update build_traffic.py

--- scripts/build_traffic.py
+++ scripts/build_traffic.py
@@ -2,13 +2,7 @@ import json
 
 data={
 "type":"FeatureCollection",
-"features":[
-{
-"type":"Feature",
-"geometry":{"type":"Point","coordinates":[8.673,52.114]},
-"properties":{"title":"Baustelle Herford Innenstadt","type":"construction"}
-}
-]
+"features":[]
 }
 
 with open("data/traffic.json","w") as f:
