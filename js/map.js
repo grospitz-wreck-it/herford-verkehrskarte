c2a3ecfe38b3d08e30084a89cd6c0dd3efa07ff9
grospitz-wreck-it <grospitz@gmail.com>
2026-03-14 19:49:24 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -7,7 +7,36 @@ attribution:"© OpenStreetMap"
 }
 ).addTo(map)
 
-const cluster = L.markerClusterGroup()
+const cluster = L.markerClusterGroup({
+
+showCoverageOnHover:false,
+
+spiderfyOnMaxZoom:true,
+
+maxClusterRadius:50,
+
+iconCreateFunction:function(cluster){
+
+const count = cluster.getChildCount()
+
+let size="small"
+
+if(count>10) size="medium"
+if(count>30) size="large"
+
+return L.divIcon({
+
+html:"<div>"+count+"</div>",
+
+className:"marker-cluster marker-cluster-"+size,
+
+iconSize:L.point(40,40)
+
+})
+
+}
+
+})
 
 map.addLayer(cluster)
 
