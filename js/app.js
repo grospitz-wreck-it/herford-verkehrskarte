907a0e18d416308afea5602d47359e45abc786f0
grospitz-wreck-it <grospitz@gmail.com>
2026-03-15 12:06:20 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,54 @@
+```javascript
 import { loadAutobahnMessages } from "./autobahn.js";
+import { loadTrafficData } from "./traffic.js";
 
 const map = L.map("map").setView([52.114, 8.673], 11);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "© OpenStreetMap"
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+attribution:"© OpenStreetMap"
 }).addTo(map);
 
-const messageLayer = L.layerGroup().addTo(map);
+
+const autobahnLayer = L.markerClusterGroup();
+const trafficLayer = L.markerClusterGroup();
+
+map.addLayer(autobahnLayer);
+map.addLayer(trafficLayer);
 
 
-// Kreisgrenzen laden
+const overlays = {
+"Autobahnmeldungen": autobahnLayer,
+"Verkehrsmeldungen": trafficLayer
+};
+
+L.control.layers(null, overlays).addTo(map);
+
 
 fetch("data/kreis-herford.geojson")
-    .then(r => r.json())
-    .then(data => {
-
-        L.geoJSON(data, {
-            style: {
-                color: "#0055ff",
-                weight: 2,
-                fillOpacity: 0
-            }
-        }).addTo(map);
-
-    });
+.then(r=>r.json())
+.then(data=>{
+L.geoJSON(data,{
+style:{
+color:"#0055ff",
+weight:2,
+fillOpacity:0
+}
+}).addTo(map);
+});
 
 
-// Autobahnmeldungen laden
+async function loadData(){
 
-loadAutobahnMessages(map, messageLayer);
+autobahnLayer.clearLayers();
+trafficLayer.clearLayers();
+
+await loadAutobahnMessages(autobahnLayer);
+await loadTrafficData(trafficLayer);
+
+}
+
+loadData();
+
+
+setInterval(loadData,120000);
+```
